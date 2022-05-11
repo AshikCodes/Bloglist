39c73fb35dc8c7eda9cb587dc606323c7ae24904
Ashik Reji <ashikreji27@gmail.com>
2022-05-12 01:20:11 +0400

    Completed 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,38 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    sum = 0;
+    for(var i = 0; i < blogs.length; i++){
+        sum += blogs[i].likes;
+    }
+    return sum;
+}
+
+const favoriteBlog = (blog) => {
+    console.log("before is",blog.map(a => a.likes) )
+    const preArray = blog.map(a => a.likes)
+    console.log("Math max thing is", Math.max(...preArray))
+    const objArr = Math.max(...preArray)
+
+    console.log("objArr is",objArr)
+
+    const favBlog = blog.filter((x) => x.likes == objArr);
+    const favBlogObj = favBlog[0]
+
+    delete favBlogObj._id;
+    delete favBlogObj.__v;
+    delete favBlogObj.url;
+
+    console.log("Fav blog is now",favBlog)
+
+
+    return favBlogObj
+
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favoriteBlog
 }
